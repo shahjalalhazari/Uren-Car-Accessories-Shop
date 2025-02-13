e1669525478062716263eb9268099fbed4291fbb
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-02-13 19:28:44 +0400

    Project setup done.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,24 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const workSans = Work_Sans({
+  subsets: ['latin'],
+  weights: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
-  title: "Uren Car Accessories Shop",
-  description: "An online car accessories shop.",
+  title: {
+    default: "Uren - Car Accessories Shop",
+    template: "%s | Uren - Car Accessories Shop"
+  },
+  description: "UREN is a cutting-edge car accessories shop dedicated to enhancing the driving experience for car enthusiasts and everyday drivers alike. The brand focuses on offering high-quality, stylish, and innovative automotive products designed to upgrade vehicle aesthetics, comfort, safety, and performance.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${workSans.className} antialiased`}
       >
         {children}
       </body>
