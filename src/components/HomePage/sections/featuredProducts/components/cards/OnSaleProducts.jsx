8fc2cca6b9d4e485ad807ff34c1057d93ef9ef85
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-02-15 22:59:10 +0400

    On Sale Products card of Featured Products section added.

--- src/components/HomePage/sections/featuredProducts/components/cards/OnSaleProducts.jsx
+++ src/components/HomePage/sections/featuredProducts/components/cards/OnSaleProducts.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import CardTitle from "../CardTitle";
+import FeaturedSingleProductCard from "@/components/shared/cards/FeaturedSingleProductCard";
 
 const OnSaleProducts = () => {
-    return (
-        <div>
-            On sale Products
-        </div>
-    );
+  return (
+    <div className="card-container">
+      {/* Card Heading */}
+      <CardTitle
+        heading={"On-Sale Products"}
+        subHeading={"On-Sale On This Week"}
+      />
+      {/* All Weekly Featured Products List */}
+      <div className="h-[68vh] overflow-y-auto mt-4">
+        {/* REPLACE */}
+        {[...Array(10)].map((_, index) => (
+          <FeaturedSingleProductCard key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OnSaleProducts;
