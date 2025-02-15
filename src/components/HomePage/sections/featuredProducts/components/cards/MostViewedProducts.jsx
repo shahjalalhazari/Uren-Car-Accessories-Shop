104cde30d2f065efcf7f8e5356d8c00e9eb578c8
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-02-15 22:57:38 +0400

    Most Viewed Products card of Featured Products section added.

--- src/components/HomePage/sections/featuredProducts/components/cards/MostViewedProducts.jsx
+++ src/components/HomePage/sections/featuredProducts/components/cards/MostViewedProducts.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import CardTitle from "../CardTitle";
+import FeaturedSingleProductCard from "@/components/shared/cards/FeaturedSingleProductCard";
 
 const MostViewedProducts = () => {
-    return (
-        <div>
-            Most Viewed Products
-        </div>
-    );
+  return (
+    <div className="card-container">
+      {/* Card Heading */}
+      <CardTitle
+        heading={"Most View Products"}
+        subHeading={"Most View On This Week"}
+      />
+      {/* All Weekly Featured Products List */}
+      <div className="h-[76vh] overflow-y-auto mt-4">
+        {/* REPLACE */}
+        {[...Array(10)].map((_, index) => (
+          <FeaturedSingleProductCard key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MostViewedProducts;
