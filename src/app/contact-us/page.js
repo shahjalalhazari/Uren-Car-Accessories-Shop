519e1a8f374ef2a3f0254bc95c462ea3ddd5cb45
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-02-25 21:54:39 +0400

    Address book on contact us page added.

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -2,6 +2,7 @@ import "./contact-us.css";
 import GoogleMap from '@/components/ContactUsPage/GoogleMap';
 import SecondaryBtn from "@/components/shared/buttons/SecondaryBtn";
 import PageBreadcrumb from '@/components/shared/PageBreadcrumb';
+import { BiEnvelope, BiHome, BiPhone } from "react-icons/bi";
 
 
 export const metadata = {
@@ -22,7 +23,7 @@ const ContactUsPage = () => {
 
           {/* Contact Form */}
           <div className="contact-form-content ">
-            <h1>Tell Us Your <span className="text-primary">Message</span></h1>
+            <h2 className="contact-page-title">Tell Us Your <span className="text-primary">Message</span></h2>
             
             <form className="contact-form">
               {/* Name Field */}
@@ -50,7 +51,23 @@ const ContactUsPage = () => {
           </div>
 
           {/* Address Book */}
-          <div className="">
+          <div className="bg-[#eee] p-8 max-h-fit">
+            <h2 className="contact-page-title"><span className="text-primary">Contact</span> Us</h2>
+            <p className="address-book-message">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram anteposuerit litterarum formas human.</p>
+            <div className="address-block ">
+              <h5><BiHome /> Address:</h5>
+              <p>233/26 Ewrat Holdings Inc. Al Waha St. Al Qouz 3. Al Quoz, Dubai - UAE.</p>
+            </div>
+            <div className="address-block">
+              <h5><BiPhone /> Number:</h5>
+              <p><span className="font-semibold">Mobile:</span> +971 58 919 6282</p>
+              <p><span className="font-semibold">Phone:</span> +971 58 919 6282</p>
+            </div>
+            <div className="address-block">
+              <h5><BiEnvelope /> E-mail:</h5>
+              <p>shahjalalhazari1@gmail.com</p>
+              <p>djangoactivation@gmail.com</p>
+            </div>
           </div>
         </div>
       </div>
