37548da270629250bcf51c6bfcf3921240ac3fb2
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-03-17 21:05:53 +0400

    Navbar bottom part added for medium devices.

--- src/components/shared/navbar/BottomNavbar.jsx
+++ src/components/shared/navbar/BottomNavbar.jsx
@@ -13,6 +13,52 @@ const BottomNavbar = ({ navItems, categories }) => {
     <div className="navbar-bottom-part">
       {/* NAVBAR BOTTOM PART FOR LARGE DEVICES */}
       {/* NAVBAR BOTTOM PART FOR MEDIUM DEVICES */}
+      <div className="md-bottom-nav">
+        <div className="relative group dropdown w-1/3">
+          <div
+            className="categories-list-heading"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <FaBars className="text-4xl" />
+            <h2 className="categories-list-heading-text">
+              Shop By
+              <br />
+              Department
+            </h2>
+            <p
+              className={`categories-arrow ${
+                isOpen ? "rotate-180" : "rotate-0"
+              }`}
+            >
+              <FaAngleDown />
+            </p>
+          </div>
+
+          {isOpen && (
+            <ul className="categories-list">
+              {categories.map((item, index) => (
+                <li key={index} className="dropdown-list-item">
+                  <Link href={`/offers/${encodeURIComponent(item.name)}`}>
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <form className="md-search-form">
+          <input
+            type="text"
+            placeholder="Search Here..."
+            className="md-search-input"
+          />
+          <button className="md-search-btn" type="submit">
+            <BiSearch className="inline-block mr-2 text-2xl" />
+            Search
+          </button>
+        </form>
+      </div>
+
       {/* NAVBAR BOTTOM PART FOR SMALL DEVICES */}
       <div className="sm-bottom-nav">
         <div className="relative w-full">
@@ -26,7 +72,7 @@ const BottomNavbar = ({ navItems, categories }) => {
               <span>Department</span>
             </h2>
             <p
-              className={`categories-arrow transition-transform duration-500 ease-in-out ${
+              className={`categories-arrow ${
                 isOpen ? "rotate-180" : "rotate-0"
               }`}
             >
@@ -37,13 +83,8 @@ const BottomNavbar = ({ navItems, categories }) => {
           {isOpen && (
             <ul className="categories-list">
               {categories.map((item, index) => (
-                <li
-                  key={index}
-                  className="dropdown-list-item"
-                >
-                  <Link
-                    href={`/offers/${encodeURIComponent(item.name)}`}
-                  >
+                <li key={index} className="dropdown-list-item">
+                  <Link href={`/offers/${encodeURIComponent(item.name)}`}>
                     {item.name}
                   </Link>
                 </li>
