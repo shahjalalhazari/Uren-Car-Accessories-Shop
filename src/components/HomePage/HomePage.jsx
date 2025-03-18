d75b6abb08eb7189dc837937b6b840fb328f0ccf
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-03-18 21:10:43 +0400

    Responsive Shopping Facilities section of home page added for all type of devices.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import "./homePage.css"
 import CarouselBanner from "./sections/CarouselBanner";
 import OfferBanners from "./sections/OfferBanners";
+import ShoppingFacilities from "./sections/ShoppingFacilities";
 
 const HomePage = () => {
   return (
     <>
       <CarouselBanner />
       <OfferBanners />
+      <ShoppingFacilities />
     </>
   );
 };
