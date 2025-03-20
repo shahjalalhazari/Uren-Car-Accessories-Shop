0b4af12eb71825aeed0f973b386275f475521b2c
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-03-20 21:22:56 +0400

    Responsive Contact Us page added for all types of devices.

---
+++ src/app/contact-us/page.js
@@ -0,0 +1,79 @@
+import GoogleMapLocations from "@/components/ContactUsPage/GoogleMapLocations";
+import SecondaryBtn from "@/components/shared/buttons/SecondaryBtn";
+import PageBreadcrumb from "@/components/shared/PageBreadcrumb";
+import { BiEnvelope, BiHome, BiPhone } from "react-icons/bi";
+import "./contact-us.css"
+
+export const metadata = {
+  title: "Contact Us",
+  keywords: ["car", "parts", "car parts", "car accessories", "toyota"],
+};
+
+const ContactUsPage = () => {
+  return (
+    <div>
+      <PageBreadcrumb breadcrumbLink={"Contact Us"} breadcrumbTitle={"Contact Us"}/>
+      <div className="uren-container mb-12 md:mb-16 lg:mb-20">
+        {/* Google Map */}
+        <GoogleMapLocations />
+
+        {/* Contact Form & Address Book */}
+        <div className="contact-layout">
+
+          {/* Contact Form */}
+          <div className="contact-form-content">
+            <h2 className="contact-page-title">Tell Us Your <span className="text-primary">Message</span></h2>
+            
+            <form className="contact-form">
+              {/* Name Field */}
+              <div className="contact-form-group">
+                <label>Your Name <span className='text-red-500'>*</span></label>
+                <input type="text" name="contactName" id="ContactName" required />
+              </div>
+              {/* Email Field */}
+              <div className="contact-form-group">
+                <label>Your Email <span className='text-red-500'>*</span></label>
+                <input type="email" name="contactEmail" id="contactEmail" required />
+              </div>
+              {/* Subject Field */}
+              <div className="contact-form-group">
+                <label>Subject <span className='text-red-500'>*</span></label>
+                <input type="text" name="contactSubject" id="contactSubject" required />
+              </div>
+              {/* Message Area */}
+              <div className="contact-form-group">
+                <label>Message <span className='text-red-500'>*</span></label>
+                <textarea name="contactMessage" id="contactMessage" required/>
+              </div>
+              <SecondaryBtn text={"Send"}/>
+            </form>
+          </div>
+
+          {/* Address Book */}
+          <div className="bg-[#eee] p-6 lg:p-8 h-fit">
+            <h2 className="contact-page-title"><span className="text-primary">Contact</span> Us</h2>
+            <p className="address-book-message">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica.</p>
+            <div className="space-y-4 lg:space-y-6">
+              <div className="address-block ">
+                <h5><BiHome /> Address:</h5>
+                <p>233/26 Ewrat Holdings Inc. Al Waha St. Al Qouz 3. Al Quoz, Dubai - UAE.</p>
+              </div>
+              <div className="address-block">
+                <h5><BiPhone /> Number:</h5>
+                <p><span className="font-semibold">Mobile:</span> +971 58 919 6282</p>
+                <p><span className="font-semibold">Phone:</span> +971 58 919 6282</p>
+              </div>
+              <div className="address-block">
+                <h5><BiEnvelope /> E-mail:</h5>
+                <p>shahjalalhazari1@gmail.com</p>
+                <p>djangoactivation@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUsPage;
