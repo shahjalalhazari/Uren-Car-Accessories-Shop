5681262c813e0489c5eed36e3f7a2470be0e17a8
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-04-02 21:39:28 +0400

    Cart page completed with responsive for all kind of devices.

--- src/app/shop/cart/page.js
+++ src/app/shop/cart/page.js
@@ -4,6 +4,7 @@ import PageBreadcrumb from '@/components/shared/PageBreadcrumb';
 import QuantitySelector from "@/components/shop/product-details/QuantitySelector";
 import { BiSolidTrash } from "react-icons/bi";
 import SecondaryBtn from "@/components/shared/buttons/SecondaryBtn";
+import Link from "next/link";
 
 const CartPage = () => {
   return (
@@ -12,15 +13,15 @@ const CartPage = () => {
           
       <div className="uren-container">
         <div className="overflow-x-auto">
-          <table className="table text-body text-sm">
+          <table className="table cart-table">
             {/* head */}
             <thead className='table-head text-base'>
               <tr>
                 <th>Image</th>
                 <th>Product</th>
-                <th className="leading-tight">Unit Price <br /><span className="text-xs opacity-70">AED</span></th>
+                <th className="leading-tight">Unit Price <br /><span className="cart-currency">AED</span></th>
                 <th>Quantity</th>
-                <th className="leading-tight">Total <br /><span className="text-xs opacity-70">AED</span></th>
+                <th className="leading-tight">Total <br /><span className="cart-currency">AED</span></th>
                 <th>Remove</th>
               </tr>
             </thead>
@@ -38,7 +39,7 @@ const CartPage = () => {
                     className="w-32 mx-auto"
                   />
                 </td>
-                <td  className="max-w-[200px] hover:text-primary uren-transition cursor-pointer">
+                <td  className="uren-transition table-product-title">
                   {item.name}
                 </td>
                 <td>{(item.price).toFixed(2)}</td>
@@ -47,7 +48,7 @@ const CartPage = () => {
                   {(item.price * item.quantity).toFixed(2)}
                 </td>
                 <td>
-                  <button className="text-xl hover:text-primary uren-transition"><BiSolidTrash /></button>
+                  <button className="uren-transition table-product-remove"><BiSolidTrash /></button>
                 </td>
               </tr>
               ))}
@@ -57,14 +58,26 @@ const CartPage = () => {
         </div>
 
         {/* CART BOTTOM PART */}
-        <div className="flex mt-6 lg:mt-8">
+        <div className="cart-btm-layout">
           {/* DISCOUNT COUPON */}
-          <form className="flex gap-x-2 ">
-            <input type="text" placeholder="Coupon Code" className="text-sm text-body px-4 py-3 outline-none border border-uren-border"/>
-            <button type="submit" className="text-base-100 font-bold bg-body px-8 py-3 text-sm uren-transition hover:bg-primary">APPLY COUPON</button>
+          <form className="coupon-code-form">
+            <input type="text" placeholder="Coupon Code" className="coupon-input"/>
+            <button type="submit" className="uren-transition coupon-form-btn">APPLY COUPON</button>
           </form>
           
           {/* CART CALCULATION */}
+          <div className="cart-total">
+            <h5>Cart Totals</h5>
+            <div className="cart-total-table">
+              <p><span>Sub Total:</span> <span>AED 150.00</span></p>
+              <p><span>Coupon:</span> <span>- AED 0.00</span></p>
+              <p className="text-dark"><span>Total:</span> <span>AED 150.00</span></p>
+            </div>
+            <Link href={"shop/checkout"}>
+              <SecondaryBtn text={"PROCEED TO CHECKOUT"} />
+            </Link>
+            {/* <button type="submit" className="text-base-100 font-bold bg-body px-6 py-3 text-sm uren-transition hover:bg-primary">PROCEED TO CHECKOUT</button> */}
+          </div>
         </div>
       </div>
     </div>
