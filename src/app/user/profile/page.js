398d2d2ca1f7b836bb8112c443f1a292ee2e1352
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-04-21 20:02:44 +0400

    Addresses, Payment Methods & Account Detailsadded for all type of devices.

--- src/app/user/profile/page.js
+++ src/app/user/profile/page.js
@@ -5,6 +5,7 @@ import PageBreadcrumb from "@/components/shared/PageBreadcrumb";
 import ATMCardDisplay from "@/components/user/profile/ATMCardDisplay";
 import Image from "next/image";
 import { FaRegStar } from "react-icons/fa";
+import PasswordField from "@/components/shared/fields/PasswordField";
 
 
 const ProfilePage = () => {
@@ -44,18 +45,17 @@ const ProfilePage = () => {
         {activeTab === "Orders" && (
           <div className="orders">
             <h6 className="profile-heading">MY ORDERS</h6>
-            <p className="body-text mt-2">Here you will get all the ordered information. <span className="hidden">For more info please click on details button.</span></p>
-              {/* ONLY FOR SMALL SCREEN */}
-              <div className="ordered-list">
+            <p className="body-text mt-4">Here you will get all the ordered information.</p>
+              <div className="ordered-list mt-6 lg:mt-8">
                 <div className="ordered-list-item">
                   <div className="item-head">
-                    <p><span className="text-dark-gray font-semibold">Order #4563</span> <br /> Ordered On: 18 Apr 2025</p>
-                    <p className="text-dark-gray font-bold lg:text-base">AED 999.99</p>
+                    <p><span className="text-highlight">Order #4563</span> <br />Date: 18 Apr 2025</p>
+                    <p className="item-price">AED 999.99</p>
                   </div>
                   <div className="item-body">
                     <div className="item-body-content">
-                      <p>Delivery On <br /> <span className="text-dark-gray">21 Apr 2025</span></p>
-                      <p className="bg-blue px-1 py-0.5 text-base-100 text-sm">Confirmed</p>
+                      <p>Delivery On <br /> <span className="text-highlight">21 Apr 2025</span></p>
+                      <div className="badge badge-soft badge-primary rounded-none">On Hold</div>
                     </div>
                     <div className="item-body-imgs">
                       <Image src={"/images/product/small-size/1.jpg"} alt="" width={60} height={60}/>
@@ -64,19 +64,19 @@ const ProfilePage = () => {
                       <Image src={"/images/product/small-size/4.jpg"} alt="" width={60} height={60}/>
                     </div>
                   </div>
-                  <div className="item-footer">
+                  <div className="item-footer uren-transition">
                     <FaRegStar /> <p>Rate Products</p>
                   </div>
                 </div>
                 <div className="ordered-list-item">
                   <div className="item-head">
-                    <p><span className="text-dark-gray font-semibold">Order #4563</span> <br /> Ordered On: 18 Apr 2025</p>
-                    <p className="text-dark-gray font-bold lg:text-base">AED 999.99</p>
+                    <p><span className="text-highlight">Order #4563</span> <br />Date: 18 Apr 2025</p>
+                    <p className="item-price">AED 999.99</p>
                   </div>
                   <div className="item-body">
                     <div className="item-body-content">
-                      <p>Delivery On <br /> <span className="text-dark-gray">21 Apr 2025</span></p>
-                      <p className="bg-blue px-1 py-0.5 text-base-100 text-sm">Confirmed</p>
+                      <p>Delivery On <br /> <span className="text-highlight">21 Apr 2025</span></p>
+                      <div className="badge badge-soft badge-primary rounded-none">On Hold</div>
                     </div>
                     <div className="item-body-imgs">
                       <Image src={"/images/product/small-size/1.jpg"} alt="" width={60} height={60}/>
@@ -85,7 +85,28 @@ const ProfilePage = () => {
                       <Image src={"/images/product/small-size/4.jpg"} alt="" width={60} height={60}/>
                     </div>
                   </div>
-                  <div className="item-footer">
+                  <div className="item-footer uren-transition">
+                    <FaRegStar /> <p>Rate Products</p>
+                  </div>
+                </div>
+                <div className="ordered-list-item">
+                  <div className="item-head">
+                    <p><span className="text-highlight">Order #4563</span> <br />Date: 18 Apr 2025</p>
+                    <p className="item-price">AED 999.99</p>
+                  </div>
+                  <div className="item-body">
+                    <div className="item-body-content">
+                      <p>Delivery On <br /> <span className="text-highlight">21 Apr 2025</span></p>
+                      <div className="badge badge-soft badge-primary rounded-none">On Hold</div>
+                    </div>
+                    <div className="item-body-imgs">
+                      <Image src={"/images/product/small-size/1.jpg"} alt="" width={60} height={60}/>
+                      <Image src={"/images/product/small-size/2.jpg"} alt="" width={60} height={60}/>
+                      <Image src={"/images/product/small-size/3.jpg"} alt="" width={60} height={60}/>
+                      <Image src={"/images/product/small-size/4.jpg"} alt="" width={60} height={60}/>
+                    </div>
+                  </div>
+                  <div className="item-footer uren-transition">
                     <FaRegStar /> <p>Rate Products</p>
                   </div>
                 </div>
@@ -94,11 +115,11 @@ const ProfilePage = () => {
         )}
 
         {activeTab === "Addresses" && (
-          <div className="addresses text-sm text-body">
+          <div className="addresses">
             <p>The following addresses will be used on the checkout page by default.</p>
-            <div className="grid grid-cols-2 gap-x-8 mt-6">
+            <div className="addresses-layout mt-6 lg:mt-8">
               <div>
-                <h6 className="text-2xl font-bold text-dark-gray uppercase mb-2">Billing Address</h6>
+                <h6 className="profile-heading mb-2">Billing Address</h6>
                 <p><strong>Apartment: </strong>Ewrat Holdings Inc. Building #26 - 2nd Floor.</p>
                 <p><strong>Street / Area: </strong>Al Waha St. Al Quoz 3.</p>
                 <p><strong>City: </strong>Al Quoz</p>
@@ -107,7 +128,7 @@ const ProfilePage = () => {
                 <p><strong>Country: </strong>Unite Arab Emirates</p>
               </div>
               <div>
-                <h6 className="text-2xl font-bold text-dark-gray uppercase mb-2">Shipping Address</h6>
+                <h6 className="profile-heading mb-2">Shipping Address</h6>
                 <p><strong>Apartment: </strong>Ewrat Holdings Inc. Building #26 - 2nd Floor.</p>
                 <p><strong>Street / Area: </strong>Al Waha St. Al Quoz 3.</p>
                 <p><strong>City: </strong>Al Quoz</p>
@@ -119,66 +140,108 @@ const ProfilePage = () => {
           </div>
         )}
 
-          {activeTab === "Payment Methods" && (
-            <>
-              <h6 className="text-2xl font-bold text-dark-gray uppercase mb-8">Saved Payment Methods</h6>
-              <div className="grid grid-cols-3 gap-x-8">
-                <div className="">
+        {activeTab === "Payment Methods" && (
+            <div className="payment-methods">
+              <h6 className="profile-heading">Saved Payment Methods</h6>
+              <div className="payment-methods-layout mt-6">
+                <div className="single-payment-card w-full">
                   <ATMCardDisplay
                   cardNumber="1234567812345678"
                   cardHolder="John Doe"
                   expiry="12/26"
                   cardType="VISA"
                     />
-                <p className="text-xs text-center mt-2 uren-transition hover:text-primary">EDIT</p>
+                  <div className="card-actions">
+                    <button className="uren-transition">EDIT</button>
+                    <button className="uren-transition">DELETE</button>
+                  </div>
+                </div>
+                <div className="single-payment-card">
+                  <ATMCardDisplay
+                  cardNumber="1234567812345678"
+                  cardHolder="Shahjalal Hazari"
+                  expiry="12/26"
+                  cardType="MASTER CARD"
+                    />
+                  <div className="card-actions">
+                    <button className="uren-transition">EDIT</button>
+                    <button className="uren-transition">DELETE</button>
+                  </div>
+                </div>
+                <div className="single-payment-card">
+                  <ATMCardDisplay
+                  cardNumber="1234567812345678"
+                  cardHolder="John Doe"
+                  expiry="12/26"
+                  cardType="VISA"
+                    />
+                  <div className="card-actions">
+                    <button className="uren-transition">EDIT</button>
+                    <button className="uren-transition">DELETE</button>
+                  </div>
                 </div>
               </div>
-              <button className="secondary-btn mt-8 place-items-center">Add New</button>
-            </>
+              <div className="w-full md:w-1/2 lg:w-1/3 mx-auto mt-6 lg:mt-8">
+                <button className="uren-transition profile-page-btn">Add New</button>
+              </div>
+            </div>
         )}
 
           {activeTab === "Account Details" && (
-            <><h6 className="text-2xl font-bold text-dark-gray uppercase mb-8">Account Details</h6>
-          <form className="account-details space-y-6">
-            <div className="grid grid-cols-2 gap-x-8">
-              <div className="form-group">
-                <label htmlFor="fname">
-                  First Name: <span>*</span>
-                </label>
-                <input
-                  type="text"
-                  name="fname"
-                  defaultValue={"Shahjalal"}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="lname">
-                  Last Name: <span>*</span>
-                </label>
-                <input
-                  type="text"
-                  name="lname"
-                  defaultValue={"Hazari"}
-                  required
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">
-                Email <span>*</span>
-              </label>
-              <input
-                type="email"
-                  name="email"
-                  defaultValue={"shahjalalhazari1@gmail.com"}
-                required
-              />
+            <div className="account-details">
+              <h6 className="profile-heading mb-6 lg:mb-8">Account Details</h6>
+              <form className="account-details-form">
+                <div className="grid md:grid-cols-2 gap-x-6 lg:gap-x-8">
+                  <div className="form-group">
+                    <label htmlFor="f-name">
+                      First Name: <span>*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="f-name"
+                      defaultValue={"Shahjalal"}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="l-name">
+                      Last Name: <span>*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="l-name"
+                      defaultValue={"Hazari"}
+                      required
+                    />
+                  </div>
                 </div>
-                <input type="submit" value="Update Details" className="secondary-btn" />
+                <div className="form-group">
+                  <label htmlFor="email">
+                    Email <span>*</span>
+                  </label>
+                  <input
+                    type="email"
+                      name="email"
+                      defaultValue={"shahjalalhazari1@gmail.com"}
+                    required
+                  />
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/3 mx-auto">
+                  <input type="submit" value="Update Changes" className="uren-transition profile-page-btn" />
+                </div>
               </form>
-                <button className="secondary-btn mt-8">Change Password</button>
-              </>
+              <h6 className="profile-heading mb-6 lg:mb-8 mt-8">Change Password</h6>
+              <form className="change-password-form">
+                <PasswordField label={"Current Password"} name={"current-password"} placeholder={"Enter Current Password"}/>
+                <div className="grid md:grid-cols-2 gap-x-6 lg:gap-x-8">
+                  <PasswordField label={"New Password"} name={"new-password"} placeholder={"Enter New Password"}/>
+                  <PasswordField label={"New Confirm Password"} name={"new-confirm-password"} placeholder={"Enter New Confirm Password"}/>
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/3 mx-auto">
+                  <input type="submit" value="Update Password" className="uren-transition profile-page-btn" />
+                </div>
+              </form>
+            </div>
         )}
 
         {activeTab === "Logout" && (
