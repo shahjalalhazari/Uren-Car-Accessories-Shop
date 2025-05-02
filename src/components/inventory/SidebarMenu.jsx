e31384fa66ca22cd76b2625604495c0f8a7183ec
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-05-02 20:05:54 +0400

    Some changes on Sidebar menu.

--- src/components/inventory/SidebarMenu.jsx
+++ src/components/inventory/SidebarMenu.jsx
@@ -1,7 +1,17 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import React from "react";
+import {
+  FaChartBar,
+  FaShoppingCart,
+  FaUsers,
+  FaDollarSign,
+  FaRegPlusSquare,
+  FaBox,
+  FaTags,
+  FaDiceD6,
+} from "react-icons/fa";
+import { TbBrandCake } from "react-icons/tb";
 
 const SidebarMenu = () => {
   const pathname = usePathname();
@@ -15,6 +25,7 @@ const SidebarMenu = () => {
               pathname === item.path && "active-menu-item"
             }`}
           >
+            {item.icon}
             {item.title}
           </li>
         </Link>
@@ -30,20 +41,54 @@ const menuItems = [
     id: 1,
     title: "Dashboard",
     path: "/inventory",
+    icon: <FaChartBar />,
   },
   {
     id: 2,
     title: "Add Products",
     path: "/inventory/add-products",
+    icon: <FaRegPlusSquare />,
   },
   {
     id: 3,
     title: "Manage Products",
     path: "/inventory/manage-products",
+    icon: <FaShoppingCart />,
   },
   {
     id: 4,
     title: "Manage Users",
     path: "/inventory/manage-users",
+    icon: <FaUsers />,
+  },
+  {
+    id: 5,
+    title: "Orders",
+    path: "/inventory/orders",
+    icon: <FaBox />,
+  },
+  {
+    id: 6,
+    title: "Sells",
+    path: "/inventory/sells",
+    icon: <FaDollarSign />,
+  },
+  {
+    id: 7,
+    title: "Categories",
+    path: "/inventory/categories",
+    icon: <FaDiceD6 />,
+  },
+  {
+    id: 8,
+    title: "Brands",
+    path: "/inventory/brands",
+    icon: <TbBrandCake />,
+  },
+  {
+    id: 9,
+    title: "Tags",
+    path: "/inventory/tags",
+    icon: <FaTags />,
   },
 ];
