58f6d3bf96d8da66fb1015c27af3c3199e9ec3ef
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-05-20 23:33:03 +0400

    bug fixed. :

--- src/app/inventory/brands/page.js
+++ src/app/inventory/brands/page.js
@@ -1,14 +1,7 @@
-import AddBrandAccordion from "@/components/inventory/brands/AddBrandAccordion";
-import AddModelAccordion from "@/components/inventory/brands/AddModelAccordion";
-
 const BrandsPage = () => {
-  // const [addBrand, setAddBrand] = useState(false);
-  // const [addCarModel, setAddCarModel] = useState(false);
 
   return (
     <div>
-      <AddBrandAccordion/>
-      <AddModelAccordion/>
       Brands & Models List
     </div>
   );
