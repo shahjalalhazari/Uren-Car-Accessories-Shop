64a6a2406e4cdf646ae8f6a9feffd097feafcb96
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-06-25 22:06:08 +0400

    Mongodb connection created.

---
+++ src/lib/connectDB.js
@@ -0,0 +1,39 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const MONGODB_URI = process.env.NEXT_MONGODB_URI;
+const DB_NAME = process.env.NEXT_MONG0DB_DB_NAME || "UrenCarAccessoriesDB";
+
+if (!MONGODB_URI) {
+    throw new Error("Mongodb URI is invalid");
+};
+
+let cachedClient = null;
+let cachedDb = null;
+
+export const connectDB = async () => {
+    if (cachedClient && cachedDb) {
+        return cachedDb;
+    };
+
+    try {
+        const client = new MongoClient(MONGODB_URI, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors:true,
+            },
+        });
+
+        await client.connect();
+        const db = client.db(DB_NAME);
+
+        cachedClient= client;
+        cachedDb = db;
+
+        console.log("Mongodb Connected Successfully!");
+        return db;
+    } catch (error) {
+        console.error("Mongodb connection error:", error);
+        throw new Error("Mongodb connection failed.")
+    }
+}
