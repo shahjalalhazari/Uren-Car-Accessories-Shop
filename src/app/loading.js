d603c340399c741d788943b7071e55d3242f9c6b
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-08-29 21:38:47 +0400

    Loading component & page added.

---
+++ src/app/loading.js
@@ -0,0 +1,5 @@
+import UrenLoading from "@/components/shared/UrenLoading";
+
+export default function Loading() {
+  return <UrenLoading/>
+}
---
+++ src/components/shared/UrenLoading.jsx
@@ -0,0 +1,9 @@
+const UrenLoading = () => {
+  return (
+    <div className="flex items-center justify-center my-12 md:my-16 lg:my-20 text-primary">
+      <span className="loading loading-infinity loading-lg"></span>
+    </div>
+  );
+};
+
+export default UrenLoading;
